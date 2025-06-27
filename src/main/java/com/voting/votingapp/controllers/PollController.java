911c96db61ba76306fc55390fe7ea8cb3e5a7226
swanyeehtet <swanyeehtet1@mytel.com.mm>
2025-06-27 10:09:31 +0630

    api developing done

--- src/main/java/com/voting/votingapp/controllers/PollController.java
+++ src/main/java/com/voting/votingapp/controllers/PollController.java
@@ -1,8 +1,9 @@
 package com.voting.votingapp.controllers;
 
 import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.request.Vote;
 import com.voting.votingapp.services.PollService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +25,17 @@ public class PollController {
     @GetMapping
     public List<Poll> getAllPolls() {
         return pollService.getAllPolls();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getPoll(@PathVariable Long id) {
+        return pollService.getPollById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/vote")
+    public void vote(@RequestBody Vote vote) {
+        pollService.vote(vote.getPollId(), vote.getOptionalIndex());
     }
 }
--- src/main/java/com/voting/votingapp/services/PollService.java
+++ src/main/java/com/voting/votingapp/services/PollService.java
@@ -1,10 +1,13 @@
 package com.voting.votingapp.services;
 
+import com.voting.votingapp.model.OptionVote;
 import com.voting.votingapp.model.Poll;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.voting.votingapp.repositories.PollRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PollService {
@@ -21,5 +24,21 @@ public class PollService {
 
     public List<Poll> getAllPolls() {
         return pollRepository.findAll();
+    }
+
+    public Optional<Poll> getPollById(Long id) {
+        return pollRepository.findById(id);
+    }
+
+    public void vote(Long pollId, int optionIndex) {
+        Poll poll = pollRepository.findById(pollId)
+                .orElseThrow(() -> new RuntimeException("Poll not found"));
+        List<OptionVote> options = poll.getOptions();
+        if(optionIndex < 0 || optionIndex >= options.size()) {
+            throw new IllegalArgumentException("Invalid option Index");
+        }
+        OptionVote selectedOption = options.get(optionIndex);
+        selectedOption.setVoteCount(selectedOption.getVoteCount() + 1);
+        pollRepository.save(poll);
     }
 }
